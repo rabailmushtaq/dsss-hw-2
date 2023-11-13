9ad74ab403daa8a3477bf751ae29362e4bd42a21
rabailmushtaq <88725712+rabailmushtaq@users.noreply.github.com>
2023-11-14 00:13:18 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math-quiz',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies your project may have
+        # For example, if you have numpy as a dependency, you can add it here
+        # 'numpy',
+    ],
+)
